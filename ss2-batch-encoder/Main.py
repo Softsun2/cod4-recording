355f2b18d1f25d5254611f69762b508072b50e84
Softsun2 <peyton.okubo13@gmail.com>
2024-06-15 20:51:12 -0500

    remove unused encoding and input format
    Atm I am the only person using this tool so I will only support my use
    case.

--- ss2-batch-encoder/Main.py
+++ ss2-batch-encoder/Main.py
@@ -3,32 +3,13 @@ import argparse, sys
 import Ss2BatchEncoder as ss2
 
 def main(args: None) -> None:
-    encoder = ss2.get_encoder(args.src_format)
-    encoder_curried = lambda path: encoder(
-        args.dest,
-        args.encoding,
-        path
-    )
-    ss2.dir_apply_encoder(args.src, encoder_curried)
+    encoder = lambda path: ss2.encode_targa(args.dest, path)
+    ss2.dir_apply_encoder(args.src, encoder)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Recursively encode exported IW3MVM videos.")
+    parser = argparse.ArgumentParser(
+        description="Recursively encode exported IW3MVM targa sequences with Xvid."
+    )
     parser.add_argument("src", type=Path, help="the directory to recursively traverse")
     parser.add_argument("dest", type=Path, help="the destination of the encoded videos")
-    parser.add_argument(
-        "--format",
-        type=str,
-        choices=["targa", "avi", "both"],
-        default="both",
-        help="the file format(s) to encode",
-        dest="src_format"
-    )
-    parser.add_argument(
-        "--encoding",
-        type=ss2.Encoding,
-        choices=["xvid", "prores"],
-        default="xvid",
-        help="the encoding"
-    )
-    
     main(parser.parse_args())
--- ss2-batch-encoder/Ss2BatchEncoder.py
+++ ss2-batch-encoder/Ss2BatchEncoder.py
@@ -1,22 +1,9 @@
 import subprocess, sys
-from pathlib import Path, PureWindowsPath
+from pathlib import Path, PurePath
 from typing import List, Callable, Union
 
 Encoding = str
 Encoder = Callable[[Path], None]
-Xvid = "xvid"
-Prores = "prores"
-Encodings = [Xvid, Prores]
-
-def get_encoder(src_format: str) -> Callable[[Path, Encoding, Path], None]:
-    if src_format == "targa":
-        return encode_targa
-    elif src_format == "avi":
-        return encoder_avi
-    elif src_format == "both":
-        return encode_both
-    else:
-        sys.exit(1)
 
 def dir_apply_encoder(directory: Path, e: Encoder) -> None:
     """
@@ -31,14 +18,18 @@ def dir_apply_encoder(directory: Path, e: Encoder) -> None:
             dir_apply_encoder(child, e)
 
 def get_start_number(files: List[str]) -> int:
+    """
+    Returns the start number given a list of targa sequence file
+    names.
+    """
     return min(list(map(lambda f: int(f[-7:]), files)))
 
-def encode_targa(dest: Path, encoding: Encoding, src: Path) -> None:
+def encode_targa(dest: Path, src: Path) -> None:
     """
     Encodes a targa sequence if `src` matches a directory with a
-    targa sequence. Exports result to `dest` using `encoding`. Returns
-    whether the operation succeeded. Assumes directory **only**
-    contains the set of targa images.
+    targa sequence. Exports result to `dest` using xvid
+    encoding. Assumes directory **only** contains the set of targa
+    images.
     """
     if not src.exists() or not src.is_dir():
         return
@@ -49,46 +40,19 @@ def encode_targa(dest: Path, encoding: Encoding, src: Path) -> None:
 
     path_names = list(map(lambda p: p.stem, paths))
     encode(
-        src / PureWindowsPath(path_names[0][:-7] + "%07d.tga"),
-        dest / PureWindowsPath(f"{src.parent.stem}-{src.stem}.avi"),
-        encoding,
+        src / PurePath(path_names[0][:-7] + "%07d.tga"),
+        dest / PurePath(f"{src.parent.stem}-{src.stem}").with_suffix(".avi"),
         get_start_number(path_names)
     )
 
-def encode_avi(dest: Path, encoding: Encoding, src: Path) -> None:
-    """
-    Encodes an avi video if `src` matches an avi video. Exports to
-    `dest` using `encoding`. Returns whether the operation succeeded.
-    """
-    if not src.exists() or not src.is_file() or src.suffix != ".avi":
-        return
-    
-    encode(src, dest, encoding)
-
-def encode_both(dest: Path, encoding: Encoding, src: Path) -> None:
-    return
-
-def encode(
-    src: Path,
-    dest: Path,
-    encoding: Encoding,
-    start_number: Union[int, None] = None
-) -> None:
-    if encoding == Xvid:
-        command = [
-            "ffmpeg",
-            "-start_number", str(start_number),
-            "-i", str(src),
-            "-vcodec", "mpeg4", "-vtag", "xvid",
-            "-g", "32",
-            "-qscale:v", "1",
-            "-y",
-            str(dest)
-        ]
-        subprocess.run(command)
-    elif encoding == Prores:
-        pass
-    else:
-        # exit
-        pass
-    return
+def encode(src: Path, dest: Path, start_number: int) -> None:
+    subprocess.run([
+        "ffmpeg",
+        "-start_number", str(start_number),
+        "-i", str(src),
+        "-vcodec", "mpeg4", "-vtag", "xvid",
+        "-g", "32",
+        "-qscale:v", "1",
+        "-y",
+        str(dest)
+    ])
